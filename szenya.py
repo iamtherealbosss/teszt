efce6c626e9b5297880e929669776764fc78b82b
IamTheBoss <90310100+iamtherealbosss@users.noreply.github.com>
2021-09-22 10:33:48 +0200

    Create szenya.py

---
+++ szenya.py
@@ -0,0 +1,9 @@
+def rantotthusosszenya(cola_type):
+    print('Hamburger zsömle.')
+    print('Belerakunk egy rántotthúst.')
+    print('Majonéz')
+    print('Saláta')
+    print('Hagyma')
+    print('Szolgáld fel ' + cola_type + 'val/vel.')
+
+rantotthusosszenya('Cola')
