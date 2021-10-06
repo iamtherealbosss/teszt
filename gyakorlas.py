9ec4e19d5a14bb3f77c031754dc0e3070087ce42
IamTheBoss <90310100+iamtherealbosss@users.noreply.github.com>
2021-10-06 10:14:39 +0200

    Create gyakorlas.py

---
+++ gyakorlas.py
@@ -0,0 +1,86 @@
+print("1. Feladat")
+
+szam = input("Add meg a neved: ")
+
+print("Szia", szam)
+
+print("2. Feladat")
+
+print("**********\n\noooooooooo")
+
+
+
+print("*o*o*o*o*o\n*o*o*o*o*o")
+
+
+print("3. Feladat")
+
+szam = input("Írj be egy számot: ")
+szam2 = input("Írj be egy számot: ")
+
+szam = int(szam)
+szam2 = int(szam2)
+
+print("Eredmény: ", szam+szam2)
+
+print("4. Feladat")
+
+szam = input("Írj be egy számot: ")
+szam2 = input("Írj be egy számot: ")
+szam3 = input("Írj be egy számot: ")
+
+szam = int(szam)
+szam2 = int(szam2)
+szam3 = int(szam2)
+
+print("Eredmény: ", szam+szam2)
+
+print("5. Feladat")
+
+szam = input("Írj be egy számot: ")
+szam2 = input("Írj be egy számot: ")
+
+szam = int(szam)
+szam2 = int(szam2)
+
+print("Eredmény: ", szam-szam2)
+
+print("6. Feladat")
+
+szam = input("Írj be egy számot: ")
+szam2 = input("Írj be egy számot: ")
+
+szam = int(szam)
+szam2 = int(szam2)
+
+print("Eredmény: ", szam*szam2)
+
+print("7. Feladat")
+
+szam = input("Írj be egy számot: ")
+szam2 = input("Írj be egy számot: ")
+
+
+szam = int(szam)
+szam2 = int(szam2)
+
+print("Eredmény: ", szam+szam2)
+
+
+print("8. Feladat")
+
+
+szam = input("Írj be egy számot: ")
+szam2 = input("Írj be egy számot: ")
+
+
+szam = int(szam)
+szam2 = int(szam2)
+szam3 = szam/szam2
+float_num = float(szam3)
+print("Eredmény: ", float_num )
+
+
+
+
+
